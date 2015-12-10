c178169ec8b2191d11e30158ad46467ad07fd2d5
dirgepye <s.stenhouse87@gmail.com>
2015-12-10 20:53:27 +0000

    Exercise 1

--- script/app.js
+++ script/app.js
@@ -1,1 +1,21 @@
 // Write your code here!
+
+$(document).ready(function() {
+    $('.announcement').append("<button class='close'>X</button>");
+
+    $('.close').click(function() {
+
+        console.log($(this));
+
+        if ($(this).text() === "X") {
+            // console.log("x");
+            $(this).parent().find("p").toggle();
+            $(this).text("show announcment");
+
+        }
+        else {
+            $(this).text("X");
+            $(this).parent().find("p").toggle();
+        }
+    });
+});
