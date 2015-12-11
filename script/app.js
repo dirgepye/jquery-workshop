00387aeba961d33d50faa793f533b7525b0bdd3c
dirgepye <s.stenhouse87@gmail.com>
2015-12-11 05:45:07 +0000

    Exercise 2

--- script/app.js
+++ script/app.js
@@ -1,1 +1,21 @@
 // Write your code here!
+$(document).ready(function() {
+
+
+    $("ul li img").on('click', function() {
+        var $img = $(this);
+        var src = $img.attr('src');
+        var alt = $img.attr('alt');
+
+        $("body").append("<div class='overlay'></div>");
+        $(".overlay").append("<img src='" + src + "'>");
+        $(".overlay").append("<p>'" + alt + "'</p>");
+        
+        
+        $(".overlay").on("click", function() {
+        $(".overlay").remove();
+
+    });
+    
+    })
+});
